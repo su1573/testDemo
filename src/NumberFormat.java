b75bbbe965f03db57183d6758edd6210c277bb52
su1573 <sph1573@163.com>
2019-02-24 16:36:00 +0800

    numFormat

---
+++ src/NumberFormat.java
@@ -0,0 +1,17 @@
+/**
+ * @program: testDemo
+ * @Date: 2019/2/24 0024 15:29
+ * @Author: Mr.SU
+ * @Description:
+ */
+public class NumberFormat {
+    private static Integer num;
+    private static Integer id;
+
+    public static void main(String[] args){
+        num = 2;
+        id = 5;
+        System.out.println(num+id);
+
+    }
+}
