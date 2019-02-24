c47675911a31a8a97921e6c279a59ce83f5cb3f1
su1573 <sph1573@163.com>
2019-02-24 16:50:13 +0800

    numFormat v 1.2

---
+++ src/testThree.java
@@ -0,0 +1,17 @@
+/**
+ * @program: testDemo
+ * @Date: 2019/2/24 0024 16:45
+ * @Author: Mr.SU
+ * @Description:
+ */
+public class testThree {
+
+    public static void main(String[] args){
+
+
+        Integer a = 10;
+        Integer b = 12;
+        System.out.println(a*b);
+
+    }
+}
