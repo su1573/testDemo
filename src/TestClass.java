23b997fff598aeca9388940abf57facdcf9c0df9
su1573 <sph1573@163.com>
2019-02-24 14:56:59 +0800

    Initial commit

---
+++ src/TestClass.java
@@ -0,0 +1,14 @@
+/**
+ * @program: testDemo
+ * @Date: 2019/2/24 0024 14:11
+ * @Author: Mr.SU
+ * @Description:
+ */
+public class TestClass {
+
+    public static  void  main(String[] args){
+
+        System.out.println("1111");
+
+    }
+}
